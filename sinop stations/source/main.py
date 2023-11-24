8ae12bb19d0e68aa942114c029764933e2bdaa67
n1tr0xs <niko4pok@gmail..com>
2023-11-24 08:51:12 +0300

    comments fixes

--- sinop stations/source/main.py
+++ sinop stations/source/main.py
@@ -19,15 +19,6 @@ class WorkerSignals(QObject):
     '''
     Defines the signals available from a running worker thread.
     Supported signals are:
-
-    finished
-        No data
-    error
-        tuple (exctype, value, traceback.format_exc() )
-    result
-        object data returned from processing
-    progress
-        ImageQt to draw
     '''
     finished = pyqtSignal()
     error = pyqtSignal(tuple)
