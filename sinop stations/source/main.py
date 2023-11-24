4d515bcad83e7bb9a189c40385acb03f6eb6b6ce
n1tr0xs <niko4pok@gmail..com>
2023-11-24 10:38:54 +0300

    empty json error handler created annotations for `get_json` fixed stations getter now in separate function term selector and getter created measurements types getter now in separate function

--- sinop stations/source/main.py
+++ sinop stations/source/main.py
@@ -7,13 +7,17 @@ from PyQt6 import QtCore, QtGui, QtWidgets
 from PyQt6.QtCore import Qt, pyqtSlot, QThreadPool, QObject, QRunnable, pyqtSignal
 from PyQt6.QtWidgets import *
 
-def get_json(page: str, parameters: dict={}, server: str='http://10.55.1.30:8640') -> dict:
+def get_json(page: str, parameters: dict={}, server: str='http://10.55.1.30:8640') -> list:
     url = f'{server}/{page}?'
     for k, v in parameters.items():
         url += f'{k}='
         url += ','.join(map(str, v)) if isinstance(v, Iterable) else str(v)
         url += '&'
-    return requests.get(url).json()
+    try:
+        return requests.get(url).json()
+    except requests.exceptions.JSONDecodeError:
+        return list()
+        
 
 class WorkerSignals(QObject):
     '''
@@ -90,16 +94,23 @@ class MainWindow(QMainWindow):
         self.label_term = QLabel('Срок:')
         self.layout.addWidget(self.label_term, 0, 0)
 
-        self.label_last_update = QLabel('Последнее обновление:')
-        self.layout.addWidget(self.label_last_update, 0, 1)
+        self.term_box = QComboBox()
+        self.layout.addWidget(self.term_box, 0, 1)
 
+        self.label_last_update = QLabel('Последнее обновление:')
+        self.layout.addWidget(self.label_last_update, 0, 2)
+    
         self.table = QTableWidget()
-        self.layout.addWidget(self.table, 1, 0, 1, 2)
+        self.layout.addWidget(self.table, 1, 0, 1, 3)
 
         self.timer = QtCore.QTimer()
         self.timer.setInterval(10*1000)
         self.timer.timeout.connect(self.start_update)
         self.timer.start()
+
+        self.get_stations()
+        self.get_terms()
+        self.get_measurements_types()
         self.start_update()
         
         self.restore_settings()
@@ -111,56 +122,65 @@ class MainWindow(QMainWindow):
         '''
         worker = Worker(self.update_data)
         self.threadpool.start(worker)
+
+    def get_stations(self):
+        '''
+        Gets station list.
+        Sets vertical header labels.
+        '''
+        self.stations = dict()
+        for row in get_json('stations.json'):
+            index, name = row['sindex'], row['station_name']
+            if name.startswith('МС'):
+                self.stations[index] = name
+        names = [
+            f'{name} [{index}]'
+            for index, name in sorted(
+                self.stations.items(),
+                key=lambda x: x[0]
+            )
+        ]
+        self.table.setColumnCount(len(names))
+        self.table.setHorizontalHeaderLabels(names)
+        
+    def get_terms(self):
+        resp = get_json('get', {'streams': 0, 'stations': self.stations.keys()})
+        self.terms = sorted(filter(bool, set(row['point_at'] for row in resp)), reverse=True)
+        for term in self.terms:
+            self.term_box.addItem(f'{dt.datetime.utcfromtimestamp(term)} UTC')
+
+    def get_measurements_types(self):
+        '''
+        Gets types of measurements.
+        Sets vertical header labels.
+        '''
+        self.bufr_name = dict()
+        for station in self.stations:
+            for row in get_json('station_taking.json', {'station': station}):
+                self.bufr_name[row['code']] = row['caption']
+        vhl = [
+            f'{name}'
+            for code, name in sorted(
+                self.bufr_name.items(),
+                key=lambda x: x[0]
+            )
+        ]
+        self.table.setRowCount(len(self.bufr_name))
+        self.table.setVerticalHeaderLabels(vhl)
     
     def update_data(self, *args, **kw):
         '''
         Gets info using REST API from server.
         Updates info in `self.table`.
         '''
-        # get time and calculate last term
-        time_step = dt.timedelta(hours=3)
-        now = dt.datetime.utcnow()
-        today = dt.datetime.utcnow().date()
-        point = dt.datetime(today.year, today.month, today.day, 0, 0, 0)
-        point += ((now-point) // time_step * time_step)
-        
-        self.label_term.setText(f'Срок: {point} UTC')
         self.label_last_update.setText(f'Обновление, подождите...')
-        # horizontal header labels
-        stations = dict()
-        for row in get_json('stations.json'):
-            index, name = row['sindex'], row['station_name']
-            if name.startswith('МС'):
-                stations[index] = name
-        names = [
-            f'{name}'
-            for index, name in sorted(
-                stations.items(),
-                key=lambda x: x[0]
-            )
-        ]
-        self.table.setColumnCount(len(names))
-        self.table.setHorizontalHeaderLabels(names)
 
-        # vertical header labels
-        bufr_name = dict()
-        for station in stations:
-            for row in get_json('station_taking.json', {'station': station}):
-                bufr_name[row['code']] = row['caption']
-        vhl = [
-            f'{name}'
-            for code, name in sorted(
-                bufr_name.items(),
-                key=lambda x: x[0]
-            )
-        ]
-        self.table.setRowCount(len(bufr_name))
-        self.table.setVerticalHeaderLabels(vhl)
-
+        point = self.terms[self.term_box.currentIndex()]
+        
         # measurements values        
         meas_for_table = dict()
-        for station in stations:
-            resp = get_json('get', {'station': station, 'streams': 0, 'point_at': point.timestamp()})
+        for station in self.stations:
+            resp = get_json('get', {'stations': station, 'streams': 0, 'point_at': point})
             for r in resp:
                 bufr = r['code']
                 station = r['station']
@@ -169,10 +189,10 @@ class MainWindow(QMainWindow):
                 if meas_for_table.get(bufr, None) is None:
                     meas_for_table[bufr] = dict()
                 meas_for_table[bufr][station] = f'{value} {unit}'
-
+                
         # update data in QTableWidget
-        for i, bufr in enumerate(sorted(bufr_name)):
-            for j, station in enumerate(sorted(stations)):
+        for i, bufr in enumerate(sorted(self.bufr_name)):
+            for j, station in enumerate(sorted(self.stations)):
                 try:
                     item = QTableWidgetItem(meas_for_table[bufr][station])
                 except KeyError:
@@ -180,7 +200,7 @@ class MainWindow(QMainWindow):
                 finally:
                     item.setFlags(item.flags() & ~Qt.ItemFlag.ItemIsEditable)
                     self.table.setItem(i, j, item)
-    
+        
         self.table.resizeColumnsToContents()
         self.table.resizeRowsToContents()
         self.label_last_update.setText(f'Последнее обновление: {dt.datetime.now()}')
